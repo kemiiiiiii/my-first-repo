3cddcbd2ea6342eef17e6a057c7df48d41852f81
kemi <supasuppa123@gmail.com>
2024-12-10 14:47:47 +0000

    pfive
    first commit

---
+++ sketch.js
@@ -0,0 +1,12 @@
+// let circleColours = ['blue', 'black', 'red', 'orange'];
+// let index;
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill('blue');
+  circle(width/2, height/2, 50);
+}
