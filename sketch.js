f686c71edf729f74bb34b161fad25af6a9c355ab
kemi <supasuppa123@gmail.com>
2024-12-10 15:11:50 +0000

    add slider

--- sketch.js
+++ sketch.js
@@ -1,12 +1,30 @@
-// let circleColours = ['blue', 'black', 'red', 'orange'];
-// let index;
+let circleColours = ['blue', 'white', 'red', 'orange'];
+let index = 0;
+let slider;
 
 function setup() {
   createCanvas(400, 400);
+  slider = createSlider(0, 350, 55, 10);
+  slider.position (10, 10);
+  slider.size(80);
 }
 
 function draw() {
-  background(220);
+  let sliderVal = slider.value();
+  background(10);
+  push();
+  fill(circleColours[index]);
+  circle(sliderVal, sliderVal, 50);
+  pop();
+  push();
   fill('blue');
-  circle(width/2, height/2, 50);
+  circle(350 - sliderVal, sliderVal, 50);
+  pop();
+}
+
+function mousePressed(){
+  index++;
+  if(index >= circleColours.length){
+    index = 0;
+  }
 }
